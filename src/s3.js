8446cd9c1703629c9d8718fe954d31f0bb16896a
Doğukan Kutluay <dogukankutluay@Dogukan-MacBook-Pro.local>
2022-11-24 11:02:03 +0300

    first commit

--- src/s3.js
+++ src/s3.js
@@ -1,17 +1,17 @@
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
 
-const PUBLIC_READ = 'public-read';
-const PRIVATE = 'private';
-const s3Client = new AWS.S3({ profile: process.env.AWS_PROFILE });
+const PUBLIC_READ = "public-read";
+const PRIVATE = "private";
+const s3Client = new AWS.S3({
+  profile: process.env.AWS_PROFILE,
+});
 
 function buildRedirectObject(bucket, from, to, options) {
   return {
-    ACL: options.private ? PRIVATE : PUBLIC_READ,
-    Body: '',
     Bucket: bucket,
-    Key: from,
-    WebsiteRedirectLocation: to
-  }
+    CopySource: "/kampanyalar/detay/63779ecb34b2b251e7c18f0d/index.html",
+    Key: "/kampanyalar/detay/63779ecb34b2b251e7c18fs0d/index.html",
+  };
 }
 
 function handleError(err, data) {
@@ -22,6 +22,15 @@ exports.handleError = handleError;
 exports.client = s3Client;
 exports.applyRedirect = (bucket, from, to, options = {}) => {
   const object = buildRedirectObject(bucket, from, to, options);
-  s3Client.putObject(object, handleError);
-  console.log("> Redirected from /" + from + " to " + to);
-}
+  s3Client.copyObject(
+    {
+      Bucket: "test.renticar.com",
+      Key: "index.html",
+      CopySource: "/kampanyalar/detay/63779ecb34b2b251e7c18f0d/index.html",
+    },
+    (e, d) => {
+      console.log(e);
+      console.log(d);
+    }
+  );
+};
